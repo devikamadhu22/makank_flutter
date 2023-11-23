e590b0715165de2b1409bd9d27e9686b0eeea18d
AdarshSeroo <144668088+AdarshSeroo@users.noreply.github.com>
2023-11-23 10:07:31 +0530

    Added pull down to refresh

--- lib/presentation/features/home/screens/home_screen.dart
+++ lib/presentation/features/home/screens/home_screen.dart
@@ -1,10 +1,10 @@
 import 'package:auto_route/annotations.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:seeroo_flutter_clean_architecture/l10n/language.dart';
 
 import '../../../../domain/entities/user.dart';
 import '../../../../l10n/lang_extensions.dart';
+import '../../../../l10n/language.dart';
 import '../../../components/common_error_loading_widget.dart';
 import '../../translation/translation_bloc/translation_bloc.dart';
 import '../blocs/home_bloc/home_bloc.dart';
@@ -26,65 +26,88 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(context.loc.title),
-        actions: [
-          IconButton(
-              onPressed: () {
-                if (context.loc.localeName == Language.en.locale.languageCode) {
-                  context.read<TranslationBloc>().add(
-                      TranslationEvent.changeLanguage(
-                          locale: Language.ar.locale));
-                } else {
-                  context.read<TranslationBloc>().add(
-                      TranslationEvent.changeLanguage(
-                          locale: Language.en.locale));
-                }
-              },
-              icon: const Icon(Icons.translate))
-        ],
-      ),
-      body: BlocListener<TranslationBloc, TranslationState>(
-        listener: (context, state) {
-          state.when(
-              initial: (_) {},
-              loading: (_) {},
-              failed: (_, __) {},
-              success: (_) {
-                ScaffoldMessenger.of(context).hideCurrentSnackBar();
-                ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                  content: Text(context.loc.language_change),
-                  backgroundColor: Colors.green,
-                ));
-              });
-        },
-        child: BlocBuilder<HomeBloc, HomeState>(
-          builder: (context, state) {
-            return state.when(
-                initial: () =>
-                    const CommonErrorLoadingWidget(error: "Failed to Call API"),
-                loading: () => const CommonErrorLoadingWidget(
-                      isLoading: true,
-                      error: null,
-                    ),
-                failed: (error) => CommonErrorLoadingWidget(error: error),
-                success: (data) {
-                  final List<User> user = data.data ?? [];
-                  return ListView.builder(
-                      itemCount: user.length,
-                      itemBuilder: (context, index) {
-                        return ListTile(
-                          title: Text(
-                            user[index].firstName ?? "",
-                            style: Theme.of(context).textTheme.headlineSmall,
-                          ),
-                        );
-                      });
-                });
-          },
-        ),
-      ),
+    return LayoutBuilder(
+      builder: (context, box) {
+        return Scaffold(
+          appBar: AppBar(
+            title: Text(context.loc.title),
+            actions: [
+              IconButton(
+                  onPressed: () {
+                    if (context.loc.localeName ==
+                        Language.en.locale.languageCode) {
+                      context.read<TranslationBloc>().add(
+                          TranslationEvent.changeLanguage(
+                              locale: Language.ar.locale));
+                    } else {
+                      context.read<TranslationBloc>().add(
+                          TranslationEvent.changeLanguage(
+                              locale: Language.en.locale));
+                    }
+                  },
+                  icon: const Icon(Icons.translate))
+            ],
+          ),
+          body: RefreshIndicator(
+            onRefresh: () async {
+              context.read<HomeBloc>().add(const HomeEvent.fetch());
+            },
+            child: ListView(
+              children: [
+                BlocListener<TranslationBloc, TranslationState>(
+                  listener: (context, state) {
+                    state.when(
+                        initial: (_) {},
+                        loading: (_) {},
+                        failed: (_, __) {},
+                        success: (_) {
+                          ScaffoldMessenger.of(context).hideCurrentSnackBar();
+                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                            content: Text(context.loc.language_change),
+                            backgroundColor: Colors.green,
+                          ));
+                        });
+                  },
+                  child: BlocBuilder<HomeBloc, HomeState>(
+                    builder: (context, state) {
+                      return state.when(
+                          initial: () => CommonErrorLoadingWidget(
+                              height: box.maxHeight,
+                              error: "Failed to Call API"),
+                          loading: () => CommonErrorLoadingWidget(
+                                height: box.maxHeight,
+                                isLoading: true,
+                                error: null,
+                              ),
+                          failed: (error) => CommonErrorLoadingWidget(
+                                error: error,
+                                height: box.maxHeight,
+                              ),
+                          success: (data) {
+                            final List<User> user = data.data ?? [];
+                            return ListView.builder(
+                                shrinkWrap: true,
+                                physics: const NeverScrollableScrollPhysics(),
+                                itemCount: user.length,
+                                itemBuilder: (context, index) {
+                                  return ListTile(
+                                    title: Text(
+                                      user[index].firstName ?? "",
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .headlineSmall,
+                                    ),
+                                  );
+                                });
+                          });
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
