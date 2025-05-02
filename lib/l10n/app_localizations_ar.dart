4636fc5fc51b7cd116d32f4eceb562a7b2c3bd94
devika <devika.madhu@seeroo.com>
2025-05-02 15:30:01 +0530

    login bloc freezed file added

---
+++ lib/l10n/app_localizations_ar.dart
@@ -0,0 +1,25 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Arabic (`ar`).
+class AppLocalizationsAr extends AppLocalizations {
+  AppLocalizationsAr([String locale = 'ar']) : super(locale);
+
+  @override
+  String get jobId => 'قورتيوي';
+
+  @override
+  String get title => 'أدفغجكل';
+
+  @override
+  String get address => 'محطة مترو كالمسري، كوتشي';
+
+  @override
+  String get date => 'إيرناكولام ساوث، كوتشي';
+
+  @override
+  String get paymentType => 'الدفع عند الاستلام';
+}
